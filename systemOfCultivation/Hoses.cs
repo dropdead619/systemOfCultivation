234f6084b2b4802b1a83b966b8146ac9d0d9ec4c
Bolsyn <Serikagis@yandex.ru>
2020-12-11 20:08:18 +0600

    Добавлен класс Шланг(Hoses)

---
+++ systemOfCultivation/Hoses.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace systemOfCultivation
+{   
+    // Шланг
+    public class Hoses
+    {
+        // Наименования шланга
+        private string name;
+
+        // Длина шланга
+        private int length;
+
+        // Внутренний диаметр шланга
+        private int insideDiameter;
+
+        // Внешний диаметр шланга
+        private int outerDiameter;
+
+        // Рабочая температура шланга
+        private int workTemperature;
+
+        // Класс шланга
+        private int classOfHoses;
+    }
+}
