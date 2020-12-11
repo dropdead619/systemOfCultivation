de44e6290205dc1fa579b4331f48ef99f913c241
wchErkhan <75239458+wchErkhan@users.noreply.github.com>
2020-12-11 21:02:13 +0600

    Add files via upload

--- systemOfCultivation/pump.cs
+++ systemOfCultivation/pump.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace systemOfCultivation
 {
+    //Насос
     class pump
     {
+        //Тип
+        private string type;
+
+        //Материал
+        private string material;
+
+        //роизводительность, л/ч
+        private int performanceLitrHour;
+
+        //Частота, Гц
+        private int frequencyHz;
+
+        //Давление, бар
+        private int PressureBar;
+
+        //Мощность, Вт
+        private int powerW;
     }
 }
--- systemOfCultivation/timer.cs
+++ systemOfCultivation/timer.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace systemOfCultivation
 {
+    //Таймер
     class timer
     {
+        //Тип
+        private string type;
+
+        //Материал
+        private string material;
+
+        //Подключение воды
+        private string waterConnection;
+
+        //Подключение шланга
+        private string hoseConnection;
+
+        //Водное давление
+        private int waterPressure;
+
+        //Водная температура	
+        private int waterTemperature;
     }
 }
