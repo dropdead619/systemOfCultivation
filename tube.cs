583a8b21552c7d6221cd36211750ee8278464877
1abiloff <58934515+1abiloff@users.noreply.github.com>
2020-12-11 20:58:50 +0600

    Труба
    Добавил класс для Магистрали труб

---
+++ tube.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace systemOfCultivation
+{
+    class Irrigation_pipe
+    {
+        private string typeOfpipe; // тип трубы железный.пвх
+
+        private double lengthOfpipe; //длина трубы
+
+        private double insideDiameter; // внутренний диаметр 
+
+        private double currentPressure; // текущее давление в трубе
+
+        private double currentTemperature; // текущая температура
+    }
+}
