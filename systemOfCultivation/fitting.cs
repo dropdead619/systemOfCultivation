588a068361ef5621a07cdee6d59ad009543fe75e
Karash-85 <75530134+Karash-85@users.noreply.github.com>
2020-12-11 21:05:41 +0600

    Add files via upload

---
+++ systemOfCultivation/fitting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanat_console
+{
+    // класс Фитинги соединительные
+    public class fitting
+    {
+        private string name;
+
+        private int quantity;
+
+        private int modelFit;
+
+    }
+}
