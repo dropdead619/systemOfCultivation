7eb94f9361dff3abb9542af67a04f6c88c99ba6b
Demezhan Kazhkenov <59953572+dropdead619@users.noreply.github.com>
2020-12-11 20:06:42 +0600

    Class container

---
+++ systemOfCultivation/container.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace systemOfCultivation
+{
+    class Container
+    {
+        private string manufacturer;
+        private int width;
+        private int height;
+        private float volume;
+    }
+}
